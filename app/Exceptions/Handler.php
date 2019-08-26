6c2b8c0dc2dbb1c89b52bcb6e42077c7d9c4a65c
Chashdaih Arizabalo <chashdaih@gmail.com>
2019-08-26 17:30:31 -0500

    posible fix session timeout

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+            return redirect('/auth/login')->with('fail', 'Tu sesión expiró. Por favor, intenta de nuevo');
+        } 
+
         return parent::render($request, $exception);
     }
 
